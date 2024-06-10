687930c873796bee7313b451af89315b5842d65d
gabesouto <soutogabriel04@gmail.com>
2024-06-10 17:22:44 -0300

    feat: migration from hard CRUD to SUPABASE - GET method and pagination logic migrated

--- src/app/api/repository/todo.ts
+++ src/app/api/repository/todo.ts
@@ -1,5 +1,6 @@
 import { HttpNotFoundError } from '@api/infra/errors'
 import { create, read, update, deleteById as dbDeleteById } from '@core/crud'
+import { Todo, TodoSchema } from '@api/schema/todo'
 
 // ========
 // TODO separar em outro arquivo
@@ -15,12 +16,12 @@ interface TodoRepositoryGetParams {
   limit?: number
 }
 
-interface Todo {
-  id: string
-  content: string
-  date: string
-  done: boolean
-}
+// interface Todo {
+//   id: string
+//   content: string
+//   date: string
+//   done: boolean
+// }
 interface TodoRepositoryGetOutput {
   todos: Todo[]
   total: number
@@ -33,35 +34,45 @@ async function get({
 }: TodoRepositoryGetParams = {}): Promise<TodoRepositoryGetOutput> {
   console.log(page, limit)
 
-  const { data, error, count } = await supabase.from('todos').select('*', {
-    count: 'exact',
-  })
+  const currentPage = page || 1
+  const currentLimit = limit || 5
+
+  const startIndex = (currentPage - 1) * currentLimit
+  const endIndex = currentPage * currentLimit - 1
+
+  const { data, error, count } = await supabase
+    .from('todos')
+    .select('*', {
+      count: 'exact',
+    })
+    .range(startIndex, endIndex)
   if (error) throw new Error('failed to fetch data from supabase')
   console.log('data', data)
 
-  //   const currentPage = page || 1
-  //   const currentLimit = limit || 10
+  const parsedData = TodoSchema.array().safeParse(data)
+  if (!parsedData.success) {
+    throw parsedData.error
+  }
+
+  const todos = parsedData.data
+  const total = count || todos.length
+
+  const totalPages = Math.ceil(total / currentLimit)
+  return {
+    todos,
+    total,
+    pages: totalPages,
+  }
 
   //   const ALL_TODOS = read().reverse()
 
-  //   const startIndex = (currentPage - 1) * currentLimit
-  //   const endIndex = currentPage * currentLimit
   //   const paginatedTodos = ALL_TODOS.slice(startIndex, endIndex)
-  //   const totalPages = Math.ceil(ALL_TODOS.length / currentLimit)
 
   //   return {
   //     todos: paginatedTodos,
   //     total: ALL_TODOS.length,
   //     pages: totalPages,
   //   }
-
-  const todos = data as Todo[]
-  const total = count || todos.length
-  return {
-    todos,
-    total,
-    pages: 1,
-  }
 }
 
 async function CreatedByContent(content: string): Promise<Todo> {
---
+++ src/app/api/schema/todo.ts
@@ -0,0 +1,14 @@
+import { z as schema } from 'zod'
+
+export const TodoSchema = schema.object({
+  id: schema.string().uuid(),
+  content: schema.string().min(1),
+  // .datetime()
+  date: schema.string().transform((date) => {
+    return new Date(date).toISOString()
+  }),
+  // .boolean()
+  done: schema.boolean(),
+})
+
+export type Todo = schema.infer<typeof TodoSchema>
